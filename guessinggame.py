28aeef80edf7118ed61ee6699f8b823db4c67d18
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2021-11-20 16:38:16 +0100

    Update guessinggame.py
    Added a scoring system and put everything in a module.

--- guessinggame.py
+++ guessinggame.py
@@ -1,21 +1,39 @@
+#Import modules
 import random
 
-# Random number
+#initiate paramaters
 random_number = random.randint(1, 100)
-guess = int(input("Guess a number between 1 and 100: "))
 score = int(100)
 
-while guess != random_number:
-    if guess > random_number:
-        print("You guessed to high, Try again!")
-        guess = int(input("Guess a number between 1 and 100: "))
-    elif guess < random_number:
-        print("You guessed to low, Try again!")
-        guess = int(input("Guess a number between 1 and 100: "))
+#Scoring function
+def scoringfunction(score):
+    scoresubstraction = random.randint(0, 10)
+    score = score -scoresubstraction
+    return score, scoresubstraction
 
-if guess == random_number:
-    print("You guessed it!")
+#main function
+def main(random_number, score):
+#get first guess
+    guess = int(input("Guess a number between 1 and 100: "))
 
-#TODO add scoring sytem
+    #loop through guesses
+    while guess != random_number:
+        if guess > random_number:
+            print("You guessed to high, Try again!")
+            score, scoresubstraction = scoringfunction(score)
+            print("Your score is now ", score, " you lost " , scoresubstraction, " points" )
+            guess = int(input("Guess a number between 1 and 100: "))
+        elif guess < random_number:
+            print("You guessed to low, Try again!")
+            score, scoresubstraction = scoringfunction(score)
+            print("Your score is now ", score, " you lost " , scoresubstraction, " points" )
+            guess = int(input("Guess a number between 1 and 100: "))
+    if guess == random_number:
+        print("You guessed it! Your score was: ", score)
+
+#call main
+main(random_number, score)
+
+#TODO Everything is a module now
 #TODO add leaderbord
 #TODO add different hints
