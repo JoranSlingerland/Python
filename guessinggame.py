2f1311172e2229560fcea94102b40796d99245be
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2021-11-22 20:38:13 +0100

    Update guessinggame.py
    fixed issues that main was beining called inside of main. This caused that we could not any code after calling main as that would be called more times then

--- guessinggame.py
+++ guessinggame.py
@@ -53,35 +53,45 @@ def high_or_low(guess, score, random_number, random_number_low, random_number_hi
         guess = int(input(f"Guess a number between {random_number_low} and {random_number_high}: "))
     return guess , score
 
-def main(scoreboard, random_number_low = 1, random_number_high = 5, score = 100):
+def main(scoreboard, input_random_number_low = 1, input_random_number_high = 5, input_score = 100):
     """Main function"""
     #temp variables
     scoreboard_filename = 'scoreboard.json'
 
     #intro to the game
     print("In this game you will be asked to gues a number between"
-    f" {random_number_low} and {random_number_high}.\nYou will start with a score and every time"
-    " you gues wrong you will lose between 0 and 10 points. \nGood luck!")
+    f" {input_random_number_low} and {input_random_number_high}.\nYou will start with a score and"
+    " every time you gues wrong you will lose between 0 and 10 points. \nGood luck!")
 
-    #get Initial information
-    username = str(input("please input your name for the leaderboard: "))
-    guess = int(input(f"Guess a number between {random_number_low} and {random_number_high}: "))
-    random_number = random.randint(random_number_low, random_number_high)
+    playing = True
 
-    #loop through guesses
-    while guess != random_number:
-        guess, score = high_or_low(guess, score, random_number,
-        random_number_low, random_number_high)
-    if guess == random_number:
-        scoreboard, scoreboard_sorted = update_scoreboard(scoreboard, score, username)
-        print(f"You guessed it! Your score was: {score}\nThe scoreboard is now:")
-        for i in scoreboard_sorted:
-            print(i[0], i[1])
+    #loop through games
+    while playing is True:
+        #set / reset values
+        random_number_low = input_random_number_low
+        random_number_high = input_random_number_high
+        score = input_score
 
-        if yes_or_no("Do you want to play again? (y/n)") is True:
-            main(scoreboard)
-        if yes_or_no("Do you want to save the scoreboard? (y/n)") is True:
-            write_dict_to_json(scoreboard, scoreboard_filename)
+        #get and set initial info
+        username = str(input("please input your name for the leaderboard: "))
+        guess = int(input(f"Guess a number between {random_number_low} and {random_number_high}: "))
+        random_number = random.randint(random_number_low, random_number_high)
+
+        #loop through gueses
+        while guess != random_number:
+            guess, score = high_or_low(guess, score, random_number,
+            random_number_low, random_number_high)
+        if guess == random_number:
+            scoreboard, scoreboard_sorted = update_scoreboard(scoreboard, score, username)
+            print(f"You guessed it! Your score was: {score}\nThe scoreboard is now:")
+            for i in scoreboard_sorted:
+                print(i[0], i[1])
+
+            playing = yes_or_no("Do you want to play again? (y/n)")
+
+    #check if you want to save the scoreboard
+    if yes_or_no("Do you want to save the scoreboard? (y/n)") is True:
+        write_dict_to_json(scoreboard, scoreboard_filename)
 
 
 #call main
